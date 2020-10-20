466af1f77d4b1c1e841ccca56c18b7f38c7edf0a
FredZeng <979738295@qq.com>
2020-10-20 18:07:35 +0800

    feat: prettify project

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-var Wxapkg = require('../');
+var Wxapkg = require('../src/index');
 
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path');
-const mkdirp = require('../mkdirp')
+const mkdirp = require('../src/mkdirp');
 
 let file = fs.readFileSync(process.argv[2]);
 
-let destination = process.argv[3]? process.argv[3] : 'wxapkg.unpack';
+let destination = process.argv[3] ? process.argv[3] : 'wxapkg.unpack';
 let wxapkg = new Wxapkg(file);
 let files = wxapkg.decode();
-files.forEach((f)=>{
-    let filePath = __dirname + '/./' + destination + f.name;
-    let dir = path.dirname(filePath);
-    mkdirp(dir, function (err) {
-        if (err) return cb(err);
+files.forEach((f) => {
+  let filePath = __dirname + '/./' + destination + f.name;
+  let dir = path.dirname(filePath);
+  mkdirp(dir, function (err) {
+    if (err) return cb(err);
 
-        console.log(filePath)
-        fs.writeFileSync(filePath, f.chunk, 'binary')
-    });
-})
+    console.log(filePath);
+    fs.writeFileSync(filePath, f.chunk, 'binary');
+  });
+});
